99ed1a73e506cc0ff1c9e1cdc19606430e792b76
Obnovlator3000 <97803837+Obnovlator3000@users.noreply.github.com>
2022-01-15 21:51:35 +0300

    Update Demotivator.py

--- simpledemotivators/Demotivator.py
+++ simpledemotivators/Demotivator.py
@@ -10,7 +10,7 @@ class Demotivator:
 
     def create(self, file: str, watermark=None, result_filename='demresult.jpg',
                font_color='white', fill_color='black',
-               font_name='times.ttf', top_size=80, bottom_size=60,
+               font_name='ICAD/simpledemotivators/times.ttf', top_size=80, bottom_size=60,
                arrange=False, use_url=False, delete_file=False) \
             -> bool:  # Returns True if method executed successfully
 
